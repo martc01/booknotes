dc61949bcc29c136f9ec7c1c91c82bda811bab38
Martc01 <kindofmart@protonmail.com>
2022-05-29 22:44:34 -0500

    Added data from JSON and Snippet mapping

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import Snippet from "./components/booknotes/Snippet";
 import Header from "./components/Header";
+import { bookNotes } from "./data/notes.js";
+import shortid from "shortid";
 
 function App() {
   return (
     <>
       <Header />
-      <Snippet
-        title="Anything you want - Derek Sivers"
-        points={10}
-        ReadingDate={10102018} //como se pasa la date?
-        ISBN={9781511366076}
-        year={2018}
-        amazon="https://www.amazon.com/Anything-You-Want-Lessons-Entrepreneur/dp/1511366079"
-        review="This book is packed with wisdom and it just makes you feel good reading it. Whether you are an entrepreneur, artist, or someone trying to figure out your next project or next theme of your life, this book is for you!"
-      />
+      {bookNotes.map((data) => (
+        <Snippet
+          key={shortid.generate()}
+          title={data.title}
+          points={data.points}
+          ReadingDate={data.readingDate} //como se pasa la date?
+          ISBN={data.ISBN}
+          year={data.year}
+          amazon={data.amazon}
+          review={data.review}
+        />
+      ))}
     </>
   );
 }
---
+++ src/data/notes.js
@@ -0,0 +1,13 @@
+export const bookNotes = [
+  {
+    title: "Anything you want - Derek Sivers",
+    points: 10,
+    readingDate: "10/10/2018",
+    ISBN: 9781511366076,
+    year: 2018,
+    amazon:
+      "https://www.amazon.com/Anything-You-Want-Lessons-Entrepreneur/dp/1511366079",
+    review:
+      "This book is packed with wisdom and it just makes you feel good reading it. Whether you are an entrepreneur, artist, or someone trying to figure out your next project or next theme of your life, this book is for you!",
+  },
+];

77e48c32ff3fe2cf26e525e3a759ff187f30c736
Martc01 <kindofmart@protonmail.com>
2022-05-30 00:02:37 -0500

    Added 2 more book notes and images

--- src/data/notes.js
+++ src/data/notes.js
@@ -10,4 +10,38 @@ export const bookNotes = [
     review:
       "This book is packed with wisdom and it just makes you feel good reading it. Whether you are an entrepreneur, artist, or someone trying to figure out your next project or next theme of your life, this book is for you!",
   },
+  {
+    title: "Remote - Jason Fried",
+    points: 5,
+    readingDate: "14/07/2019",
+    ISBN: 780804137508,
+    year: 2013,
+    amazon:
+      "https://www.amazon.com/Remote-Office-Required-Jason-Fried/dp/0804137501",
+    review:
+      "A paradigm-smashing, compulsively readable case for a radically remote workplace.",
+  },
+  {
+    title: "How to learn any language - Barry Farber",
+    points: 2,
+    readingDate: "09/04/2017",
+    ISBN: 9781567315431,
+    year: 2002,
+    amazon:
+      "https://www.amazon.com/How-Learn-Any-Language-Inexpensively/dp/0806512717",
+    review:
+      "Barry Farber has been a true adventurer in languages for forty-six years and can speak in twenty-five tongues. The techniques he presents will have you speaking, reading, writing and enjoying any foreign language you want to learn - or have to learn - in a surprisingly short time.Without beating your head against verb conjugations or noun declensions, you can follow Farber's principles and glide toward proficiency in your chosen language.",
+  },
 ];
+
+/*{
+    title: '',
+    points: ,
+    readingDate: "",
+    ISBN: ,
+    year: ,
+    amazon:
+      "",
+    review:
+      "",
+  },*/
